330a894a5602bb4625146bdad8f6e9d1512cccf6
Chisanan232 <chi10211201@cycu.org.tw>
2020-06-29 16:52:05 +0800

    Modify the project directory-file constructor tree

--- CrawlPart.scala
+++
@@ -1,9 +0,0 @@
-package Cafe_GoogleMap_Crawler
-
-sealed trait CrawlPart
-
-case object Basic extends CrawlPart
-case object Services extends CrawlPart
-case object Comments extends CrawlPart
-case object Images extends CrawlPart
-case object All extends CrawlPart
