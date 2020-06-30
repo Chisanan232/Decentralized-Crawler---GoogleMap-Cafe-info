86cd877a9d10cf447dc495f73946f5d6385e6919
Chisanan232 <chi10211201@cycu.org.tw>
2020-06-30 15:56:34 +0800

    Add initial-code about building Cassandra connector

--- src/main/scala/Paladin/DataSaverPaladin.scala
+++ src/main/scala/Paladin/DataSaverPaladin.scala
@@ -4,8 +4,18 @@ import Cafe_GoogleMap_Crawler.src.main.scala.config._
 
 import akka.actor.{Actor, ActorLogging}
 
+import org.apache.spark.{SparkConf, SparkContext}
+import com.datastax.spark.connector._
+
 
 class DataSaverPaladin extends Actor with ActorLogging {
+
+  val conf = new SparkConf(true)
+    .set("spark.cassandra.connection.host", CassandraConfig.CassandraHost)
+    .setMaster(CassandraConfig.CassandraMaster)
+    .setAppName(CassandraConfig.CassandraAppName)
+  val sc = new SparkContext(conf)
+
 
   override def receive: Receive = {
 
