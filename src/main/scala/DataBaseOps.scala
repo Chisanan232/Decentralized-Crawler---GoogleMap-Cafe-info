0911a98904bf7a3db6e13ca2d24fe0aa78ca3d79
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-01 16:05:47 +0800

    Add a new method 'getTables'

--- src/main/scala/DataBaseOps.scala
+++ src/main/scala/DataBaseOps.scala
@@ -4,6 +4,8 @@ import Cafe_GoogleMap_Crawler.src.main.scala.config.CassandraConfig
 
 import com.datastax.driver.core.Cluster
 
+import scala.collection.mutable.ListBuffer
+
 
 class DataBaseOps {
 
@@ -11,9 +13,24 @@ class DataBaseOps {
   // Should use withoutJMXReporting to avoid error ""
   // https://docs.datastax.com/en/developer/java-driver/3.5/manual/metrics/
 
-  val cluster = Cluster.builder()
+  private val cluster = Cluster.builder()
     .withoutJMXReporting()
     .addContactPoint(CassandraConfig.CassandraHost)
     .build()
+
+  def getTables(keyspace: String): List[String] = {
+    val tablesInfo = this.cluster.getMetadata().getKeyspace(keyspace).getTables()
+    val tables = tablesInfo.toString.split(";,")
+
+    var newTablesInfo = new ListBuffer[String]()
+
+    for((ele, index) <- tables.zipWithIndex) {
+      if (index.equals(0)) {newTablesInfo += ele.toString.drop(1)}
+      else if (index.equals(newTables.length - 1)) {newTablesInfo += ele.toString.filterNot(";]".toSet)}
+      else {newTablesInfo += ele}
+    }
+
+    newTablesInfo.toList
+  }
 
 }
