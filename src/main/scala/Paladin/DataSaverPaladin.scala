385ec1d6a68051a7473dddafa036f2b08a7f7f39
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-01 17:20:43 +0800

    Announce some methods to be private

--- src/main/scala/Paladin/DataSaverPaladin.scala
+++ src/main/scala/Paladin/DataSaverPaladin.scala
@@ -1,6 +1,7 @@
 package Cafe_GoogleMap_Crawler.src.main.scala.Paladin
 
 import Cafe_GoogleMap_Crawler.src.main.scala.{Basic, Services, Comments, Images}
+import Cafe_GoogleMap_Crawler.src.main.scala.DataBaseOps
 import Cafe_GoogleMap_Crawler.src.main.scala.config._
 
 import org.json4s.jackson.JsonMethods._
@@ -14,24 +15,29 @@ import com.datastax.spark.connector._
 
 class DataSaverPaladin extends Actor with ActorLogging {
 
-  val conf = new SparkConf(true)
+  // Save data to database Cassandra has 2 methods
+  // Method 1
+  private val conf = new SparkConf(true)
     .set("spark.cassandra.connection.host", CassandraConfig.CassandraHost)
     .setMaster(CassandraConfig.CassandraMaster)
     .setAppName(CassandraConfig.CassandraAppName)
-  val sc = new SparkContext(conf)
+  private val sc = new SparkContext(conf)
 
   private def parseData(data: String): Map[String, Any] = {
     implicit val dataFormatter = DefaultFormats
     parse(data).extract[Map[String, Any]]
   }
 
+  // Method 2
+  private val dbo = new DataBaseOps
 
-  private def writeData(keyspace: String, table: String, data: Map[String, Any]): Unit = {
+  private def writeData(keyspace: String, table: String, data: String): Unit = {
     // 1. Define a Seq type value
     // 2. Parse the target value to filter data we want
     // 3. Add into Seq type value
     // 4. parallelize by Spark method
-    val sparkRDDData = this.sc.parallelize()
+
+    val sparkRDDData = this.sc.parallelize(data)
     val columns = table match {
       case "Basic" => SomeColumns("isClosed", "title", "address", "phone", "url", "businessHours", "rating", "googlemap", "id", "createdAt")
       case "Services" => SomeColumns("services", "googlemap", "id", "createdAt")
@@ -39,7 +45,7 @@ class DataSaverPaladin extends Actor with ActorLogging {
       case "Images" => SomeColumns("photos", "googlemap", "id", "createdAt")
       case _ => SomeColumns("key", "value")
     }
-    sparkRDDData.saveToCassandra(keyspace, table, columns)
+//    sparkRDDData.saveToCassandra(keyspace, table, columns)
   }
 
 
@@ -53,7 +59,7 @@ class DataSaverPaladin extends Actor with ActorLogging {
       log.info("Receive the crawl-result data!")
 
       val mapData = this.parseData(data)
-      this.writeData("target_keyspace", part.toString, mapData)
+      this.writeData("target_keyspace", part.toString, data)
 
   }
 
