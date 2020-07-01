07efbca7ef84f7597df2310eebba8ab4f13f89bf
Chisanan232 <chi10211201@cycu.org.tw>
2020-07-01 15:28:25 +0800

    Add initial code about building Cassandra object

--- src/main/scala/DataBaseOps.scala
+++ src/main/scala/DataBaseOps.scala
@@ -1,8 +1,19 @@
 package Cafe_GoogleMap_Crawler.src.main.scala
 
+import Cafe_GoogleMap_Crawler.src.main.scala.config.CassandraConfig
+
 import com.datastax.driver.core.Cluster
 
 
 class DataBaseOps {
+
+  // Create Cluster object
+  // Should use withoutJMXReporting to avoid error ""
+  // https://docs.datastax.com/en/developer/java-driver/3.5/manual/metrics/
+
+  val cluster = Cluster.builder()
+    .withoutJMXReporting()
+    .addContactPoint(CassandraConfig.CassandraHost)
+    .build()
 
 }
